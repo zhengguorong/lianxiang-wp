3bc5b22ccd0efcb4a7595e8ba26075960fc199ca
zhengguorong <zhengguorong@xinshi.net>
2015-03-30 15:29:30 +0800

    修复无法显示title问题

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title></title>
     <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/lib/bootstrap/bootstrap.min.css"/>
     <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/lib/bootstrap/bootstrap-theme.min.css"/>
     <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/lib/swiper/idangerous.swiper.css"/>
